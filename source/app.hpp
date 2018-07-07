c569c191d14c8d1beb57dbe375a3ded51bea6a2c
PyroFlareX <31327816+PyroFlareX@users.noreply.github.com>
2018-07-07 15:46:01 -0700

    Branch

---
+++ source/app.hpp
@@ -0,0 +1,19 @@
+#ifndef APP_HPP
+#define APP_HPP
+
+#include <switch.h>
+
+#include <SDL2/SDL.h>
+
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
+
+class App
+{
+public:
+	int WindowInit(window);
+	//void winDestroy(void);
+	//void scanInput(void);
+};
+
+#endif
