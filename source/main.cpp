8a42b034bdd35c52e469aa456ffe2750b7dd9751
PyroFlareX <31327816+PyroFlareX@users.noreply.github.com>
2018-07-07 14:32:21 -0700

    Include main loop

--- source/main.cpp
+++ source/main.cpp
@@ -31,6 +31,17 @@ int main(int argc, char* args[]) {
         }
 		
 		//Main loop
+		while (appletMainLoop()){
+		//Scan all the inputs.
+        hidScanInput();
+
+        //Code
+
+        //hidKeysDown returns information about which buttons have been just pressed (and they weren't in the previous frame)
+        u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
+
+        if (kDown & KEY_PLUS) break; // break in order to return to hbmenu
+		}
 		
         SDL_DestroyWindow(window);
         SDL_Quit();
