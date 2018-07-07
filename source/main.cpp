f3cafb9084899aa88abceeae5c8142be6d0ae5e7
PyroFlareX <31327816+PyroFlareX@users.noreply.github.com>
2018-07-07 15:44:28 -0700

    fix

--- source/main.cpp
+++ source/main.cpp
@@ -2,16 +2,34 @@
 #include <stdio.h>
 
 #include <switch.h>
+
 #include <SDL2/SDL.h>
 
-#include "app.hpp"
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
 
 int main(int argc, char* args[]) {
-		SDL_Window* window = NULL;
+	//Window pointer /refrence
+        SDL_Window* window = NULL;
+	//IDK
+        SDL_Surface* screenSurface = NULL;
+//Initializing and if unsuccessful, it exits
+        if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+                fprintf(stderr, "could not initialize sdl2: %s\n", SDL_GetError());
+                return 1;
+        }
+
+        window = SDL_CreateWindow(
+                "hello_sdl2",
+                SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                SCREEN_WIDTH, SCREEN_HEIGHT,
+                SDL_WINDOW_SHOWN
+        );
+        if (window == NULL) {
+                fprintf(stderr, "could not create window: %s\n", SDL_GetError());
+                return 1;
+        }
 		
-		App game;
-		game.WindowInit(window);
-	
 		//Main loop
 		while (appletMainLoop()){
 		//Scan all the inputs.
@@ -27,5 +45,5 @@ int main(int argc, char* args[]) {
 		
         SDL_DestroyWindow(window);
         SDL_Quit();
-		return 0;
+	return 0;
 }
